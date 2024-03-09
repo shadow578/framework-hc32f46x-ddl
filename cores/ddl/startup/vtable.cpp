893dc6c20ab3a7068f8b598b0edda87e0b4c25c0
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-03-09 14:18:15 +0100

    vector table struct

--- cores/ddl/startup/vtable.cpp
+++ cores/ddl/startup/vtable.cpp
@@ -1,5 +1,93 @@
 #include <stdint.h>
 
+/**
+ * @brief IRQ handler pointer
+ */
+typedef void (*irq_vector_t)(void);
+
+/**
+ * @brief vector table definition of HC32F460
+ */
+typedef struct
+{
+    /**
+     * @brief top of stack
+     */
+    uint32_t *stackTop;
+
+    /**
+     * @brief reset handler
+     */
+    irq_vector_t reset;
+
+    /**
+     * @brief NMI handler
+     */
+    irq_vector_t nmi;
+
+    /**
+     * @brief hard fault handler
+     */
+    irq_vector_t hardFault;
+
+    /**
+     * @brief MPU fault handler
+     */
+    irq_vector_t memManageFault;
+
+    /**
+     * @brief bus fault handler
+     */
+    irq_vector_t busFault;
+
+    /**
+     * @brief usage fault handler
+     */
+    irq_vector_t usageFault;
+
+    /**
+     * @brief reserved, 4 x 32bit
+     */
+    uint32_t reserved1[4];
+
+    /**
+     * @brief SVCall handler
+     */
+    irq_vector_t svCall;
+
+    /**
+     * @brief debug monitor handler
+     */
+    irq_vector_t debugMonitor;
+
+    /**
+     * @brief reserved, 1 x 32bit
+     */
+    uint32_t reserved2;
+
+    /**
+     * @brief PendSV handler
+     */
+    irq_vector_t pendSV;
+
+    /**
+     * @brief SysTick handler
+     */
+    irq_vector_t sysTick;
+
+    /**
+     * @brief IRQ handlers, total 144 vectors
+     */
+    irq_vector_t irqs[144];
+} vector_table_t;
+
+/**
+ * @brief check the size of irq_vector_t and uint32_t match the expectations
+ */
+static_assert(sizeof(irq_vector_t) == 4, "irq_vector_t must be 4 bytes");
+static_assert(sizeof(uint32_t) == 4, "uint32_t must be 4 bytes");
+static_assert(sizeof(vector_table_t) == (16 + 144) * 4, "vector_table_t does not match expected size");
+
 extern "C"
 {
     extern uint32_t __StackTop;
@@ -163,168 +251,165 @@ extern "C"
     extern void IRQ143_Handler(void);
 }
 
-void *vector_table[] __attribute__((section(".vectors"), aligned(2))) =
+vector_table_t vector_table __attribute__((section(".vectors"), aligned(2))) =
     {
-        (void *)&__StackTop,
-        (void *)Reset_Handler,
-        (void *)NMI_Handler,
-        (void *)HardFault_Handler,
-        (void *)MemManage_Handler,
-        (void *)BusFault_Handler,
-        (void *)UsageFault_Handler,
-        (void *)nullptr,
-        (void *)nullptr,
-        (void *)nullptr,
-        (void *)nullptr,
-        (void *)SVC_Handler,
-        (void *)DebugMon_Handler,
-        (void *)nullptr,
-        (void *)PendSV_Handler,
-        (void *)SysTick_Handler,
+        .stackTop = &__StackTop,
+        .reset = Reset_Handler,
+        .nmi = NMI_Handler,
+        .hardFault = HardFault_Handler,
+        .memManageFault = MemManage_Handler,
+        .busFault = BusFault_Handler,
+        .usageFault = UsageFault_Handler,
+        .svCall = SVC_Handler,
+        .debugMonitor = DebugMon_Handler,
+        .pendSV = PendSV_Handler,
+        .sysTick = SysTick_Handler,
 
         // IRQs
-        (void *)IRQ000_Handler,
-        (void *)IRQ001_Handler,
-        (void *)IRQ002_Handler,
-        (void *)IRQ003_Handler,
-        (void *)IRQ004_Handler,
-        (void *)IRQ005_Handler,
-        (void *)IRQ006_Handler,
-        (void *)IRQ007_Handler,
-        (void *)IRQ008_Handler,
-        (void *)IRQ009_Handler,
-        (void *)IRQ010_Handler,
-        (void *)IRQ011_Handler,
-        (void *)IRQ012_Handler,
-        (void *)IRQ013_Handler,
-        (void *)IRQ014_Handler,
-        (void *)IRQ015_Handler,
-        (void *)IRQ016_Handler,
-        (void *)IRQ017_Handler,
-        (void *)IRQ018_Handler,
-        (void *)IRQ019_Handler,
-        (void *)IRQ020_Handler,
-        (void *)IRQ021_Handler,
-        (void *)IRQ022_Handler,
-        (void *)IRQ023_Handler,
-        (void *)IRQ024_Handler,
-        (void *)IRQ025_Handler,
-        (void *)IRQ026_Handler,
-        (void *)IRQ027_Handler,
-        (void *)IRQ028_Handler,
-        (void *)IRQ029_Handler,
-        (void *)IRQ030_Handler,
-        (void *)IRQ031_Handler,
-        (void *)IRQ032_Handler,
-        (void *)IRQ033_Handler,
-        (void *)IRQ034_Handler,
-        (void *)IRQ035_Handler,
-        (void *)IRQ036_Handler,
-        (void *)IRQ037_Handler,
-        (void *)IRQ038_Handler,
-        (void *)IRQ039_Handler,
-        (void *)IRQ040_Handler,
-        (void *)IRQ041_Handler,
-        (void *)IRQ042_Handler,
-        (void *)IRQ043_Handler,
-        (void *)IRQ044_Handler,
-        (void *)IRQ045_Handler,
-        (void *)IRQ046_Handler,
-        (void *)IRQ047_Handler,
-        (void *)IRQ048_Handler,
-        (void *)IRQ049_Handler,
-        (void *)IRQ050_Handler,
-        (void *)IRQ051_Handler,
-        (void *)IRQ052_Handler,
-        (void *)IRQ053_Handler,
-        (void *)IRQ054_Handler,
-        (void *)IRQ055_Handler,
-        (void *)IRQ056_Handler,
-        (void *)IRQ057_Handler,
-        (void *)IRQ058_Handler,
-        (void *)IRQ059_Handler,
-        (void *)IRQ060_Handler,
-        (void *)IRQ061_Handler,
-        (void *)IRQ062_Handler,
-        (void *)IRQ063_Handler,
-        (void *)IRQ064_Handler,
-        (void *)IRQ065_Handler,
-        (void *)IRQ066_Handler,
-        (void *)IRQ067_Handler,
-        (void *)IRQ068_Handler,
-        (void *)IRQ069_Handler,
-        (void *)IRQ070_Handler,
-        (void *)IRQ071_Handler,
-        (void *)IRQ072_Handler,
-        (void *)IRQ073_Handler,
-        (void *)IRQ074_Handler,
-        (void *)IRQ075_Handler,
-        (void *)IRQ076_Handler,
-        (void *)IRQ077_Handler,
-        (void *)IRQ078_Handler,
-        (void *)IRQ079_Handler,
-        (void *)IRQ080_Handler,
-        (void *)IRQ081_Handler,
-        (void *)IRQ082_Handler,
-        (void *)IRQ083_Handler,
-        (void *)IRQ084_Handler,
-        (void *)IRQ085_Handler,
-        (void *)IRQ086_Handler,
-        (void *)IRQ087_Handler,
-        (void *)IRQ088_Handler,
-        (void *)IRQ089_Handler,
-        (void *)IRQ090_Handler,
-        (void *)IRQ091_Handler,
-        (void *)IRQ092_Handler,
-        (void *)IRQ093_Handler,
-        (void *)IRQ094_Handler,
-        (void *)IRQ095_Handler,
-        (void *)IRQ096_Handler,
-        (void *)IRQ097_Handler,
-        (void *)IRQ098_Handler,
-        (void *)IRQ099_Handler,
-        (void *)IRQ100_Handler,
-        (void *)IRQ101_Handler,
-        (void *)IRQ102_Handler,
-        (void *)IRQ103_Handler,
-        (void *)IRQ104_Handler,
-        (void *)IRQ105_Handler,
-        (void *)IRQ106_Handler,
-        (void *)IRQ107_Handler,
-        (void *)IRQ108_Handler,
-        (void *)IRQ109_Handler,
-        (void *)IRQ110_Handler,
-        (void *)IRQ111_Handler,
-        (void *)IRQ112_Handler,
-        (void *)IRQ113_Handler,
-        (void *)IRQ114_Handler,
-        (void *)IRQ115_Handler,
-        (void *)IRQ116_Handler,
-        (void *)IRQ117_Handler,
-        (void *)IRQ118_Handler,
-        (void *)IRQ119_Handler,
-        (void *)IRQ120_Handler,
-        (void *)IRQ121_Handler,
-        (void *)IRQ122_Handler,
-        (void *)IRQ123_Handler,
-        (void *)IRQ124_Handler,
-        (void *)IRQ125_Handler,
-        (void *)IRQ126_Handler,
-        (void *)IRQ127_Handler,
-        (void *)IRQ128_Handler,
-        (void *)IRQ129_Handler,
-        (void *)IRQ130_Handler,
-        (void *)IRQ131_Handler,
-        (void *)IRQ132_Handler,
-        (void *)IRQ133_Handler,
-        (void *)IRQ134_Handler,
-        (void *)IRQ135_Handler,
-        (void *)IRQ136_Handler,
-        (void *)IRQ137_Handler,
-        (void *)IRQ138_Handler,
-        (void *)IRQ139_Handler,
-        (void *)IRQ140_Handler,
-        (void *)IRQ141_Handler,
-        (void *)IRQ142_Handler,
-        (void *)IRQ143_Handler,
+        .irqs = {
+            IRQ000_Handler,
+            IRQ001_Handler,
+            IRQ002_Handler,
+            IRQ003_Handler,
+            IRQ004_Handler,
+            IRQ005_Handler,
+            IRQ006_Handler,
+            IRQ007_Handler,
+            IRQ008_Handler,
+            IRQ009_Handler,
+            IRQ010_Handler,
+            IRQ011_Handler,
+            IRQ012_Handler,
+            IRQ013_Handler,
+            IRQ014_Handler,
+            IRQ015_Handler,
+            IRQ016_Handler,
+            IRQ017_Handler,
+            IRQ018_Handler,
+            IRQ019_Handler,
+            IRQ020_Handler,
+            IRQ021_Handler,
+            IRQ022_Handler,
+            IRQ023_Handler,
+            IRQ024_Handler,
+            IRQ025_Handler,
+            IRQ026_Handler,
+            IRQ027_Handler,
+            IRQ028_Handler,
+            IRQ029_Handler,
+            IRQ030_Handler,
+            IRQ031_Handler,
+            IRQ032_Handler,
+            IRQ033_Handler,
+            IRQ034_Handler,
+            IRQ035_Handler,
+            IRQ036_Handler,
+            IRQ037_Handler,
+            IRQ038_Handler,
+            IRQ039_Handler,
+            IRQ040_Handler,
+            IRQ041_Handler,
+            IRQ042_Handler,
+            IRQ043_Handler,
+            IRQ044_Handler,
+            IRQ045_Handler,
+            IRQ046_Handler,
+            IRQ047_Handler,
+            IRQ048_Handler,
+            IRQ049_Handler,
+            IRQ050_Handler,
+            IRQ051_Handler,
+            IRQ052_Handler,
+            IRQ053_Handler,
+            IRQ054_Handler,
+            IRQ055_Handler,
+            IRQ056_Handler,
+            IRQ057_Handler,
+            IRQ058_Handler,
+            IRQ059_Handler,
+            IRQ060_Handler,
+            IRQ061_Handler,
+            IRQ062_Handler,
+            IRQ063_Handler,
+            IRQ064_Handler,
+            IRQ065_Handler,
+            IRQ066_Handler,
+            IRQ067_Handler,
+            IRQ068_Handler,
+            IRQ069_Handler,
+            IRQ070_Handler,
+            IRQ071_Handler,
+            IRQ072_Handler,
+            IRQ073_Handler,
+            IRQ074_Handler,
+            IRQ075_Handler,
+            IRQ076_Handler,
+            IRQ077_Handler,
+            IRQ078_Handler,
+            IRQ079_Handler,
+            IRQ080_Handler,
+            IRQ081_Handler,
+            IRQ082_Handler,
+            IRQ083_Handler,
+            IRQ084_Handler,
+            IRQ085_Handler,
+            IRQ086_Handler,
+            IRQ087_Handler,
+            IRQ088_Handler,
+            IRQ089_Handler,
+            IRQ090_Handler,
+            IRQ091_Handler,
+            IRQ092_Handler,
+            IRQ093_Handler,
+            IRQ094_Handler,
+            IRQ095_Handler,
+            IRQ096_Handler,
+            IRQ097_Handler,
+            IRQ098_Handler,
+            IRQ099_Handler,
+            IRQ100_Handler,
+            IRQ101_Handler,
+            IRQ102_Handler,
+            IRQ103_Handler,
+            IRQ104_Handler,
+            IRQ105_Handler,
+            IRQ106_Handler,
+            IRQ107_Handler,
+            IRQ108_Handler,
+            IRQ109_Handler,
+            IRQ110_Handler,
+            IRQ111_Handler,
+            IRQ112_Handler,
+            IRQ113_Handler,
+            IRQ114_Handler,
+            IRQ115_Handler,
+            IRQ116_Handler,
+            IRQ117_Handler,
+            IRQ118_Handler,
+            IRQ119_Handler,
+            IRQ120_Handler,
+            IRQ121_Handler,
+            IRQ122_Handler,
+            IRQ123_Handler,
+            IRQ124_Handler,
+            IRQ125_Handler,
+            IRQ126_Handler,
+            IRQ127_Handler,
+            IRQ128_Handler,
+            IRQ129_Handler,
+            IRQ130_Handler,
+            IRQ131_Handler,
+            IRQ132_Handler,
+            IRQ133_Handler,
+            IRQ134_Handler,
+            IRQ135_Handler,
+            IRQ136_Handler,
+            IRQ137_Handler,
+            IRQ138_Handler,
+            IRQ139_Handler,
+            IRQ140_Handler,
+            IRQ141_Handler,
+            IRQ142_Handler,
+            IRQ143_Handler,
+        },
 };
