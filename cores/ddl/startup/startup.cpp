fac15b06d4a689a239daab90302a4496fd9062ce
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-03-09 15:07:18 +0100

    cleanup startup WIP

---
+++ cores/ddl/startup/startup.cpp
@@ -0,0 +1,98 @@
+#include "startup.h"
+#include <hc32f460.h>
+
+volatile char stack[DDL_STACK_SIZE] __attribute__((section(".stack"), aligned(8)));
+volatile char heap[DDL_HEAP_SIZE] __attribute__((section(".heap"), aligned(8)));
+
+//
+// reset handler implementation
+//
+#define __ALWAYS_INLINE __attribute__((always_inline)) inline
+
+__ALWAYS_INLINE void initDataSection()
+{
+    // copy .data from ROM to RAM
+    register uint32_t *src = &__etext;
+    register uint32_t *dst = &__data_start__;
+    register uint32_t *end = &__data_end__;
+    for (; dst < end; dst++, src++)
+    {
+        *dst = *src;
+    }
+}
+
+__ALWAYS_INLINE void initRetDataSection()
+{
+    // copy .retdata from ROM to RAM
+    register uint32_t *src = &__etext_ret_ram;
+    register uint32_t *dst = &__data_start_ret_ram__;
+    register uint32_t *end = &__data_end_ret_ram__;
+    for (; dst < end; src++, dst++)
+    {
+        *dst = *src;
+    }
+}
+
+__ALWAYS_INLINE void initBssSection()
+{
+    // clear .bss
+    register uint32_t *dst = &__bss_start__;
+    register uint32_t *end = &__bss_end__;
+    for (; dst < end; dst++)
+    {
+        *dst = 0;
+    }
+}
+
+__ALWAYS_INLINE void initRetBssSection()
+{
+    // clear .retbss
+    register uint32_t *dst = &__bss_start_ret_ram__;
+    register uint32_t *end = &__bss_end_ret_ram__;
+    for (; dst < end; dst++)
+    {
+        *dst = 0;
+    }
+}
+
+/**
+ * @brief set SRAM3 wait states
+ */
+__ALWAYS_INLINE void setSRAM3Wait()
+{
+    M4_SRAMC->WTPR = 0x77;
+    M4_SRAMC->CKPR = 0x77;
+    M4_SRAMC->WTCR = 0x1100;
+    M4_SRAMC->WTPR = 0x76;
+    M4_SRAMC->CKPR = 0x76;
+}
+
+extern "C" __attribute__((naked, used, optimize("O0"))) void Reset_Handler(void)
+{
+    __asm__ __volatile__(
+        // set stack pointer
+        "ldr sp, =__StackTop\n"
+
+        // branch to C++ reset handler
+        "b Reset_Handler_C\n");
+}
+
+extern "C" void Reset_Handler_C(void)
+{
+    // FIXME: ret_ram broken again ...
+    
+    initDataSection();
+    // initRetDataSection();
+
+    initBssSection();
+    // initRetBssSection();
+
+    setSRAM3Wait();
+
+    SystemInit();
+    __libc_init_array();
+    main();
+
+    while (1)
+        ;
+}
---
+++ cores/ddl/startup/startup.h
@@ -0,0 +1,128 @@
+#ifndef __STARTUP_H__
+#define __STARTUP_H__
+#include <stdint.h>
+#include "ld_symbols.h"
+#include "interrupts.h"
+
+#ifndef DDL_STACK_SIZE
+#define DDL_STACK_SIZE 0x00000400
+#endif
+
+#ifndef DDL_HEAP_SIZE
+#define DDL_HEAP_SIZE 0x00000C00
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    /**
+     * @brief DDL system initialization
+     */
+    extern void SystemInit(void);
+
+    /**
+     * @brief libc array init
+     */
+    extern void __libc_init_array(void);
+
+    /**
+     * @brief application entry point
+     */
+    extern int main(void);
+
+    /**
+     * @brief stack memory
+     */
+    extern volatile char stack[];
+
+    /**
+     * @brief heap memory
+     */
+    extern volatile char heap[];
+
+    /**
+     * @brief IRQ handler pointer
+     */
+    typedef void (*irq_vector_t)(void);
+
+    /**
+     * @brief vector table definition of HC32F460
+     */
+    typedef struct
+    {
+        /**
+         * @brief top of stack
+         */
+        uint32_t *stackTop;
+
+        /**
+         * @brief reset handler
+         */
+        irq_vector_t reset;
+
+        /**
+         * @brief NMI handler
+         */
+        irq_vector_t nmi;
+
+        /**
+         * @brief hard fault handler
+         */
+        irq_vector_t hardFault;
+
+        /**
+         * @brief MPU fault handler
+         */
+        irq_vector_t memManageFault;
+
+        /**
+         * @brief bus fault handler
+         */
+        irq_vector_t busFault;
+
+        /**
+         * @brief usage fault handler
+         */
+        irq_vector_t usageFault;
+
+        /**
+         * @brief reserved, 4 x 32bit
+         */
+        uint32_t reserved1[4];
+
+        /**
+         * @brief SVCall handler
+         */
+        irq_vector_t svCall;
+
+        /**
+         * @brief debug monitor handler
+         */
+        irq_vector_t debugMonitor;
+
+        /**
+         * @brief reserved, 1 x 32bit
+         */
+        uint32_t reserved2;
+
+        /**
+         * @brief PendSV handler
+         */
+        irq_vector_t pendSV;
+
+        /**
+         * @brief SysTick handler
+         */
+        irq_vector_t sysTick;
+
+        /**
+         * @brief IRQ handlers, total 144 vectors
+         */
+        irq_vector_t irqs[144];
+    } vector_table_t;
+
+#ifdef __cplusplus
+}
+#endif
+#endif // __STARTUP_H__
