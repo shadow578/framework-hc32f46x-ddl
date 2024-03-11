e40fd6aa2673f8e6ee1d3be9d2363b0189f6281e
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-03-11 10:24:25 +0100

    utils and more checks added

---
+++ cores/ddl/startup/utils.h
@@ -0,0 +1,54 @@
+#ifndef __MEMORY_UTILS_H__
+#define __MEMORY_UTILS_H__
+#include <stdint.h>
+#include "startup.h"
+
+extern "C" char *_sbrk(int incr);
+
+/**
+ * @brief get the current stack pointer
+ * @return the current stack pointer
+ */
+static inline void *get_stack_pointer()
+{
+    register void *sp asm("sp");
+    return sp;
+}
+
+/**
+ * @brief get the total available stack space
+ * @return the total stack space in bytes
+ */
+static inline size_t get_total_stack_space()
+{
+    return (size_t)&__StackTop - (size_t)&__StackLimit;
+}
+
+/**
+ * @brief get the free stack space
+ * @return the free stack space in bytes
+ */
+static inline size_t get_free_stack_space()
+{
+    return (size_t)get_stack_pointer() - (size_t)&__StackLimit;
+}
+
+/**
+ * @brief get the total heap space
+ * @return the total heap space in bytes
+ */
+static inline size_t get_total_heap_space()
+{
+    return (size_t)&__HeapLimit - (size_t)&__HeapBase;
+}
+
+/**
+ * @brief get the total available heap space
+ * @return the total heap space in bytes
+ */
+static inline size_t get_free_heap_space()
+{
+    return (size_t)&__HeapLimit - (size_t)_sbrk(0);
+}
+
+#endif // __MEMORY_UTILS_H__
